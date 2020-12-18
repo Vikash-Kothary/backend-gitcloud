61ef03518eb38ad75cabc34f7b4c78cd86fd3925
Vikash Kothary <kothary.vikash@gmail.com>
2020-12-18 19:29:51 +0000

    Add a unit test for the Index Controller

---
+++ modules/core/src/test/java/com/vikashkothary/gitcloud/controller/IndexControllerTest.java
@@ -0,0 +1,36 @@
+package com.vikashkothary.gitcloud.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class IndexControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void getHello() throws Exception {
+        final String expectedResponse = "Hello Git Cloud";
+
+        mockMvc.perform(get("/api/v1")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo(expectedResponse)));
+
+    }
+}
