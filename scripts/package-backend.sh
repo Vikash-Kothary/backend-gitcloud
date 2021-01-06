46908c31fae167e2e01db9d6df707eebb8af04ca
Vikash Kothary <kothary.vikash@gmail.com>
2021-01-06 21:41:43 +0000

    Remove SNAPSHOT from the versions

--- scripts/package-backend.sh
+++ scripts/package-backend.sh
@@ -3,9 +3,11 @@
 # description: Build release zip.
 
 PACKAGE_FOLDER=target/dist
-PACKAGE_VERSION=${GITCLOUD_BACKEND_VERSION}-SNAPSHOT
 PACKAGE_JAR=modules/core/target/gitcloud-core-${PACKAGE_VERSION}.jar
-PACKAGE_NAME="gitcloud-${PACKAGE_VERSION}.zip"
+
+PACKAGE_NAME=backend-gitcloud
+PACKAGE_VERSION=${GITCLOUD_BACKEND_VERSION}
+PACKAGE_FILENAME=${PACKAGE_NAME}-${PACKAGE_VERSION}.zip
 
 echo "Building release package."
 echo "--- Create release folder if it doesn't exist: ${PACKAGE_FOLDER}"
@@ -15,4 +17,4 @@ echo "--- Copy relevant files into dist folder."
 cp "${PACKAGE_JAR}" "${PACKAGE_FOLDER}"
 
 echo "--- Build release zip."
-zip "$(dirname ${PACKAGE_FOLDER})/${PACKAGE_NAME}" "${PACKAGE_FOLDER}"
+zip "$(dirname ${PACKAGE_FOLDER})/" "${PACKAGE_FOLDER}"
