2d87841fcee5693df229080797a74ee9830f3701
Vikash Kothary <kothary.vikash@gmail.com>
2021-01-06 20:40:15 +0000

    Add script to build a release package

--- scripts/package-backend.sh
+++ scripts/package-backend.sh
@@ -2,5 +2,17 @@
 # file: package-backend.sh
 # description: Build release zip.
 
-echo "TODO"
-exit 1
+PACKAGE_FOLDER=target/dist
+PACKAGE_VERSION=${GITCLOUD_BACKEND_VERSION}-SNAPSHOT
+PACKAGE_JAR=modules/core/target/gitcloud-core-${PACKAGE_VERSION}.jar
+PACKAGE_NAME="gitcloud-${PACKAGE_VERSION}.zip"
+
+echo "Building release package."
+echo "--- Create release folder if it doesn't exist: ${PACKAGE_FOLDER}"
+mkdir -p ${PACKAGE_FOLDER}
+
+echo "--- Copy relevant files into dist folder."
+cp "${PACKAGE_JAR}" "${PACKAGE_FOLDER}"
+
+echo "--- Build release zip."
+zip "$(dirname ${PACKAGE_FOLDER})/${PACKAGE_NAME}" "${PACKAGE_FOLDER}"
